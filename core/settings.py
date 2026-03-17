8a21890e29011e7a10275efd4941fed22c62fecc
Luis Enrique Sanchez Romero <luiskike2210@gmail.com>
2026-03-17 12:58:48 -0600

    admin styles 2

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-SSTATIC_URL = 'static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost:3000').split(',')
